2b19a185322656da553d2bc99f793188a917ba36
BryghtShadow <bryghtshadow@gmail.com>
2021-06-28 18:14:45 +1200

    refactor as per review

--- RunesQuery.lua
+++ RunesQuery.lua
@@ -1,13 +1,15 @@
 local util_args = require('Module:ArgsUtil')
 local util_cargo = require('Module:CargoUtil')
 local util_esports = require('Module:EsportsUtil')
+local util_html = require('Module:HtmlUtil')
 local util_table = require('Module:TableUtil')
 local util_text = require('Module:TextUtil')
 local util_vars = require('Module:VarsUtil')
-local m_tab_vars = require('Module:TabVariables')
+local i18n = require('Module:I18nUtil')
+local TabVariables = require('Module:TabVariables')
 
--- Maybe move this to /data
-local RUNE_FORGED = {
+-- Runes Reforged data defined here only because Rune Trees are not stored in cargo yet.
+local RUNES_REFORGED = {
   {
     name = "Domination",
     slots = {
@@ -198,8 +200,38 @@ local RUNE_FORGED = {
   }
 }
 
+local INTRO_COLUMNS = {
+	'Patch', 'Player', 'Team', 'Champion', 'Position',
+	'Primary', 'Secondary',
+}
+local RUNES_COLUMNS = {
+	'Keystone', 'Rune1', 'Rune2', 'Rune3', 'Rune4', 'Rune5',
+	'Stat1', 'Stat2', 'Stat3',
+}
+local OUTRO_COLUMNS = { 'MatchHistory' }
+local COLUMNS = util_table.mergeArrays(INTRO_COLUMNS, RUNES_COLUMNS, OUTRO_COLUMNS)
+
 local p = {}
 local h = {}
+
+function p.test()
+	return p._main{
+		overviewPage = 'LCS/2021 Season/Spring Season',
+		page = 1
+	}
+end
+
+-- "Helper" function to serialize the tree names, so that they can be stored in cargo.
+-- {{#invoke:RunesQuery|treename|Grasp of the Undying,Demolish,Bone Plating,Overgrowth,Biscuit Delivery,Time Warp Tonic,Adaptive Force,Adaptive Force,Armor}}
+-- returns: {"Primary":"Resolve","Secondary":"Inspiration"}
+function p.treename(frame)
+	local args = util_args.merge()
+	local runeNames = args[1] or args.runes
+	if not runeNames then return end
+	local trees = h.getRuneTreeNamesFromRuneNames(runeNames)
+	-- Serialize
+	return mw.text.jsonEncode(trees)
+end
 
 function p.main(frame)
 	local args = util_args.merge()
@@ -207,10 +239,23 @@ function p.main(frame)
 end
 
 function p._main(args)
-	local tabNumber = args.page or m_tab_vars.getIndex() or 1
+	local query = h.getQuery(args)
+	local data = util_cargo.queryAndCast(query)
+	util_map.rowsInPlace(data, h.processRow)
+  return h.makeOutput(data)
+end
+
+function h.getQuery(args)
+	local tabNumber = args.page or TabVariables.getIndex() or 1
 	local overviewPage = util_esports.getOverviewPage(args.overviewPage)
-	local rows = util_cargo.queryAndCast{
-		tables = 'ScoreboardGames=SG, MatchSchedule=MS, ScoreboardTeams=ST, ScoreboardPlayers=SP, Teams',
+	local ret = {
+		tables = {
+			'ScoreboardGames=SG',
+			'MatchSchedule=MS',
+			'ScoreboardTeams=ST',
+			'ScoreboardPlayers=SP',
+			'Teams',
+    },
 		join = {
 			'SG.MatchId = MS.MatchId',
 			'SG.UniqueGame = ST.UniqueGame',
@@ -218,81 +263,43 @@ function p._main(args)
 			'ST.Team = Teams.Name',
 		},
 		fields = {
-			'SG.N_Page',
-			'SG.N_MatchInTab',
-			'SG.N_MatchInPage',
-			'SG.N_GameInMatch',
-			'SG.Patch = V',
+			'SG.Patch',
 			'SP.Name = Player',
 			'COALESCE(Teams.Short,Teams.Name,ST.Team) = Team',
-			'SP.Champion = Champion',
+			'SP.Champion',
 			'SP.IngameRole = Position',
 			-- 'SP.KeystoneMastery=Keystone', -- unused ??
-			'SP.Runes = Runes', -- keystone, 5 runes, 3 stats
-			'SG.MatchHistory = MH',
+			'SP.Runes', -- keystone, 5 runes, 3 stats
+			'SG.MatchHistory',
 		},
 		where = {
 			('SG.OverviewPage = "%s"'):format(overviewPage),
-			'SG.MatchHistory IS NOT NULL',
+			('SG.MatchHistory IS NOT NULL'),
 			('SG.N_Page = %d'):format(tabNumber),
 		},
-		orderBy = 'SG.N_Page, SG.N_MatchInPage, SG.N_MatchInTab, SG.N_GameInMatch',
-	}
-	local headers = {
-		'V', 'Player', 'Team', 'Champion', 'Position', 'Tree', 'Secondary',
-		'Keystone', 'Rune 1', 'Rune 2', 'Rune 3', 'Rune 4', 'Rune 5',
-		'Stat 1', 'Stat 2', 'Stat 3', 'MH',
-	}
-	local root = mw.html.create()
-	h.printTable(root, headers, rows)
-	return tostring(root)
+		orderBy = 'MS.N_Page, MS.N_MatchInPage, SG.N_GameInMatch',
+	}	
 end
 
-function h.printTable(root, headers, rows)
-	local tbl = root:tag('table'):addClass('wikitable runedata hoverable-rows')
-	h.printHeaders(tbl, headers)
-	h.printRows(tbl, headers, rows)
-end
-
-function h.printHeaders(tbl, headers)
-	local tr = tbl:tag('tr'):addClass('runedata-header')
-	for _, s in ipairs(headers) do
-		tr:tag('th'):wikitext(s)
-	end
-end
-
-function h.printRows(tbl, headers, rows)
-	for i, row in ipairs(rows) do
-		local tr = tbl:tag('tr')
-		tr:addClass('rune-line-'..row.Position)
-		row.MH = row.MH:gsub("^http://", "https://")
-		row.MH = '['..row.MH.. ' Link]'
-		h.parseRuneTrees(row)
-		for _, k in ipairs(headers) do
-			tr:tag('td'):wikitext(row[k])
-		end
-	end
+function h.processRow(row)
+	row.classes = {'rune-line-'..row.Position} -- required for toggle
+	row.MatchHistory = util_text.extLink(row.MatchHistory:gsub("^http://", "https://")) -- ensure HTTPS
+	h.parseRuneTrees(row)
 end
 
 function h.parseRuneTrees(row)
-	local runes = row.Runes
-	local getTree = function(runeName)
-		for _, runePath in ipairs(RUNE_FORGED) do
-			for _, slot in ipairs(runePath.slots) do
-				for _, rune in ipairs(slot.runes) do
-					if rune.name == runeName then
-						return runePath.name
-					end
-				end
-			end
-		end
+	local trees = h.getRuneTreeNamesFromRuneNames(row.Runes)
+	util_table.merge(row, trees)
+	for i, k in ipairs(RUNES_COLUMNS) do
+		row[k] = runes[i]
 	end
-	if type(runes) == 'string' then
-		runes = util_text.split(runes)
-	end
+end
+
+function h.getRuneTreeNamesFromRuneNames(runeNames)
+	local runeNames = util_text.splitIfString(runeNames)
 	local treeTally = {}
-	for _, runeName in ipairs(runes) do
-		local treeName = getTree(runeName)
+	for _, runeName in ipairs(runeNames) do
+		local treeName = h.getRuneTreeNameFromRuneName(runeName)
 		if treeName ~= nil then
 			treeTally[treeName] = (treeTally[treeName] or 0) + 1
 		end
@@ -301,22 +308,50 @@ function h.parseRuneTrees(row)
 	for name, count in pairs(treeTally) do
 		t[#t+1] = {count=count, name=name}
 	end
+	-- Sort from most to least
 	table.sort(t, function(a,b) return a.count > b.count end)
-	row.Tree = t[1].name
-	row.Secondary = t[2].name
-	for i, k in ipairs({
-		'Keystone', 'Rune 1', 'Rune 2', 'Rune 3', 'Rune 4', 'Rune 5',
-		'Stat 1', 'Stat 2', 'Stat 3',
-	}) do
-		row[k] = runes[i]
+	return {
+		Primary = t[1].name,
+		Secondary = t[2].name,
+	}
+end
+
+function h.getRuneTreeNameFromRuneName(runeName)
+	for _, runePath in ipairs(RUNES_REFORGED) do
+		for _, slot in ipairs(runePath.slots) do
+			for _, rune in ipairs(slot.runes) do
+				if rune.name == runeName then
+					return runePath.name
+				end
+			end
+		end
 	end
 end
 
-function p.test()
-	return p._main{
-		overviewPage = 'LCS/2021 Season/Spring Season',
-		page = 1
-	}
+-- Output
+function h.makeOutput(data)
+  local output = mw.html.create()
+	local tbl = root:tag('table')
+		:addClass('wikitable runedata hoverable-rows')
+	h.printHeaders(tbl)
+	h.printRows(tbl, data)
+  return tostring(output)
+end
+
+function h.printHeaders(tbl)
+	local tr = util_html.printHeaderFromI18n(tbl, COLUMNS)
+		:addClass('runedata-header')
+end
+
+function h.printRows(tbl, data)
+	for i, row in ipairs(data) do
+		local tr = tbl:tag('tr')
+			:addClass()
+		for _, k in ipairs(COLUMNS) do
+			tr:tag('td')
+				:wikitext(row[k])
+		end
+	end
 end
 
 return p
