e7dc8d2387cf99cdfbcb4034885ad2fcb64db059
BryghtShadow <bryghtshadow@gmail.com>
2021-07-12 13:10:18 +1200

    Move summary to top of file, as per review

--- scoreboard_player_runes.py
+++ scoreboard_player_runes.py
@@ -1,15 +1,17 @@
-import re
-import json
-
 from mwcleric.fandom_client import FandomClient
 from mwcleric.auth_credentials import AuthCredentials
 from mwcleric.template_modifier import TemplateModifierBase
 from mwparserfromhell.nodes import Template
 from mwparserfromhell.nodes.extras.parameter import Parameter
-
 from runes_reforged_parser import RunesReforgedParser
 
+
+credentials = AuthCredentials(user_file='bot')
+site = FandomClient(wiki='lol', credentials=credentials)
+summary = "add primary rune tree"
+
 rune_parser = RunesReforgedParser()
+
 
 class TemplateModifier(TemplateModifierBase):
     def update_template(self, template: Template):
@@ -29,16 +31,7 @@ class TemplateModifier(TemplateModifierBase):
                              before='secondary')
         return
 
-def add_primary_rune_tree():
-    credentials = AuthCredentials(user_file='bot')
-    site = FandomClient(wiki='lol', credentials=credentials)
 
-    summary = "add primary rune tree"
-
-    TemplateModifier(site, 'Scoreboard/Player',
-                     page_list=site.pages_using('Scoreboard/Player/Runes'),
-                     summary=summary).run()
-
-
-if __name__ == '__main__':
-    add_primary_rune_tree()
+TemplateModifier(site, 'Scoreboard/Player',
+                 page_list=site.pages_using('Scoreboard/Player/Runes'),
+                 summary=summary).run()
