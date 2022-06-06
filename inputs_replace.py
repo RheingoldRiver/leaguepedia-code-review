ba0281ba7e54d78850df4b4c107002f873c178d7
ArbolitoLoco <36609914+arbolitoloco1@users.noreply.github.com>
2022-06-06 00:40:55 -0300

    Update inputs_replace.py

--- inputs_replace.py
+++ inputs_replace.py
@@ -1,10 +1,11 @@
 from mwrogue.esports_client import EsportsClient
 from mwrogue.auth_credentials import AuthCredentials
 from mwcleric.errors import RetriedLoginAndStillFailed
+from mwclient.errors import MaximumRetriesExceeded
 import mwparserfromhell
 
 credentials = AuthCredentials(user_file="bot")
-site = EsportsClient("lol", credentials=credentials, max_retries=0)
+site = EsportsClient("lol", credentials=credentials, max_retries_mwc=2, max_retries=2, retry_interval=10)
 
 response = site.cargo_client.query(
     tables="Teamnames=T, Teamnames__Inputs=I, Teamnames__Inputs=I2",
@@ -14,17 +15,27 @@ response = site.cargo_client.query(
     group_by="T._ID"
 )
 
+failed_pages = []
+
 for item in response:
     for page in site.client.pages[item["Link"]].backlinks():
-        page_text = page.text()
-        page_wikitext = mwparserfromhell.parse(page_text)
+        original_text = page.text()
+        page_wikitext = mwparserfromhell.parse(original_text)
         for template in page_wikitext.filter_templates():
             for param in template.params:
                 if param.value.lower() == item["BadInput"]:
                     template.add(param.name, item["GoodInput"])
-        if str(page_text) != str(page_wikitext):
+        if original_text != str(page_wikitext):
+            print(page.name)
             try:
-                site.save(page=page, text=str(page_wikitext), summary="Changing team inputs with comma")
+                site.save_title(title=page.name, text=str(page_wikitext), summary="Changing team inputs with comma")
             except RetriedLoginAndStillFailed:
+                failed_pages.append(page.name)
+                pass
+            except MaximumRetriesExceeded:
+                failed_pages.append(page.name)
                 pass
             print(f"Saved {page.name}")
+
+print("Failures:")
+print('\n'.join(failed_pages))
